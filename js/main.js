b63860276422f71f357c27e25dd7cbe048d9ac0d
seejayi <seejayi@yandex.ru>
2015-01-27 23:46:17 +0200

    [*] slider revolution add

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-(function() {
-	'use strict'
+// (function() {
+// 	'use strict'
 
-	$(document).ready(function(){
+// 	$(document).ready(function(){
 
-	// var sliderControl = $('.dotstyle li');
+// 	// var sliderControl = $('.dotstyle li');
 		
-		$('.glamour-bxslider').bxSlider({
-			mode: 'vertical',
-			slideMargin: 10,
-			// useCSS: false,
-			// slideWidth: 800,
-	    	// minSlides: 2,
-	    	// maxSlides: 5
-			controls: false
-			// pagerCustom: '.dotstyle',	
+// 		$('.glamour-bxslider').bxSlider({
+// 			mode: 'vertical',
+// 			slideMargin: 10,
+// 			// useCSS: false,
+// 			// slideWidth: 800,
+// 	    	// minSlides: 2,
+// 	    	// maxSlides: 5
+// 			controls: false
+// 			// pagerCustom: 'dotstyle'	
 
-		});
+// 		});
 
-		// $(".dotstyle li").click(function() {
-		// 	$('.dotstyle li').removeClass('current');
-		// 	$(this).addClass('current');
-		// });
+// 		// $(".dotstyle li").click(function() {
+// 		// 	$('.dotstyle li').removeClass('current');
+// 		// 	$(this).addClass('current');
+// 		// });
 
-	// BxSlider options:
-			// pagerCustom: '.dotstyle',	**Parent element to be used as the pager.Parent element must contain a data-slide-index="x" element for each slide.Not for use with dynamic carousels**
-			// slideSelector: '',		**jquery selector**
-			// infiniteLoop: true,		**transition when clicking next**
-			// hideControlOnEnd: false,		**addClass 'disabled', if infiniteLoop=true**
-			// easing: null,		**for during css transition**
-			// captions: false,		**Img caption.Captions are derived from the image's title attribute**
-			// adaptiveHeight: false,		**Dynamically adjust slider height based on each slide's height**
-			// adaptiveHeightSpeed: '500', **Transition duration speed(ms)**
-			// video: fasle,		**include a video**
-			// responsive: true,		**auto-resizing**
-			// wrapperClass: 'bxslider',		**to change slider-wrap**
-			// pagerSelector: '',		**Element used to populate the populate the pager**
-			// buildPager: null,		**[options: function(slideIndex)]If supplied,function is called on every slide element,and the returned value is used as the pager item markup**
-			// nextSelector: null,		**Element used to populate the "Next" control**
-			// prevSelector: null,		**[options: jquery selector]...**
-	// **	
-	});
+// 	// BxSlider options([mode: default]):
+// 			// pagerCustom: '.dotstyle',	**Parent element to be used as the pager.Parent element must contain a data-slide-index="x" element for each slide.Not for use with dynamic carousels**
+// 			// slideSelector: '',		**jquery selector**
+// 			// infiniteLoop: true,		**transition when clicking next**
+// 			// hideControlOnEnd: false,		**addClass 'disabled', if infiniteLoop=true**
+// 			// easing: null,		**for during css transition**
+// 			// captions: false,		**Img caption.Captions are derived from the image's title attribute**
+// 			// adaptiveHeight: false,		**Dynamically adjust slider height based on each slide's height**
+// 			// adaptiveHeightSpeed: '500', **Transition duration speed(ms)**
+// 			// video: fasle,		**include a video**
+// 			// responsive: true,		**auto-resizing**
+// 			// wrapperClass: 'bxslider',		**to change slider-wrap**
+// 			// pagerSelector: '',		**Element used to populate the populate the pager**
+// 			// buildPager: null,		**[options: function(slideIndex)]If supplied,function is called on every slide element,and the returned value is used as the pager item markup**
+// 			// nextSelector: null,		**Element used to populate the "Next" control**
+// 			// prevSelector: null,		**[options: jquery selector]...**
+// 	// **	
+// 	});
 
-})();
+// })();
